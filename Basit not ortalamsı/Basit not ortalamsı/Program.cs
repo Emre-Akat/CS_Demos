d054f811af9a569555905f68f1ef8049dab5ec7b
Emre-Akat <yunusemreakattt@gmail.com>
2024-12-27 07:41:01 +0300

    UploadProject
    Kullanıcının 5 farklı dersin notunu girmesi istenir ve bu notların ortalamasını hesaplar. Program, öğrencinin not ortalamasına göre geçip geçmediğini belirtir

---
+++ Basit not ortalamsı/Basit not ortalamsı/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Basit_not_ortalamsı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //öğrencinin notlarının tutulacağı bir dizi tanımladık
+            double[] notlar=new double[5];
+            //dizimize eleman girdisi aldık.
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                Console.WriteLine("Notu giriniz :");
+                notlar[i]=Convert.ToDouble(Console.ReadLine());
+            }
+
+            GectiMiKontrol(notlar);
+
+            Console.ReadKey();
+        }
+        //ortalama hesaplayacak metotdumuz
+        public static double OrtalamaHesaplayici(double[] notlar)
+        {
+            double top = 0;
+            foreach (double d in notlar)
+            {
+                top += d;
+            }
+            //ortalama değeri döndürüyoruz.
+            return top / notlar.Length;
+        }
+        // öğrencinin not ortalamasına göre geçti ,kaldı yazdıran metot
+        public static void GectiMiKontrol(double[] notlar)
+        { 
+              if(OrtalamaHesaplayici(notlar)>=50)
+            {
+                Console.WriteLine("Geçti");
+            }
+              else
+            {
+                Console.WriteLine("Kaldı");
+            }
+        }
+    }
+}
---
+++ Basit not ortalamsı/Readme.md
@@ -0,0 +1,15 @@
+# Not Ortalaması Hesaplama
+
+## Açıklama
+ Kullanıcının 5 farklı dersin notunu girmesi istenir ve bu notların ortalamasını hesaplar. Program, öğrencinin not ortalamasına göre geçip geçmediğini belirtir.
+
+## Özellikler
+- Kullanıcıdan 5 adet ders notu girişi yapılır.
+- Girilen notların ortalaması hesaplanır.
+- Hesaplanan ortalamaya göre öğrenciye "Geçti" veya "Kaldı" mesajı gösterilir.
+- Program, basit bir kullanıcı arayüzü sunar ve kolayca anlaşılabilir şekilde çalışır.
+
+
+
+### Girdi
+Kullanıcıdan 5 ders notu girilmesi istenir:
