c6d39b161b5600f83cd1b9e3e175578988a0b265
Emre-Akat <yunusemreakattt@gmail.com>
2024-12-27 08:44:46 +0300

    UploadProject
    Bu program, kullanıcıdan bir sayı alarak, bu sayının asal olup olmadığını kontrol eder. Asal sayılar yalnızca 1 ve kendisiyle tam bölünebilen sayılardır. Program, sayının asal olup olmadığını belirleyip kullanıcıya uygun bir mesaj gösterir.

---
+++ asal sayi kontrolu/Readme.md
@@ -0,0 +1,15 @@
+# Asal Sayı Kontrolü
+
+## Açıklama
+Bu program, kullanıcıdan bir sayı alarak, bu sayının asal olup olmadığını kontrol eder. Asal sayılar yalnızca 1 ve kendisiyle tam bölünebilen sayılardır. Program, sayının asal olup olmadığını belirleyip kullanıcıya uygun bir mesaj gösterir.
+
+## Özellikler
+- Kullanıcıdan bir sayı alınır.
+- Girilen sayının asal olup olmadığı kontrol edilir.
+- Eğer sayı asal ise, kullanıcıya "Asaldır" mesajı gösterilir.
+- Eğer sayı asal değilse, kullanıcıya "Asal değildir" mesajı gösterilir.
+- Sayılar 1 ve daha küçük olanlar asal olarak kabul edilmez.
+
+---
+
+
---
+++ asal sayi kontrolu/asal sayi kontrolu/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace asal_sayi_kontrolu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir sayı giriniz :");
+            int kullaniciSayi=int.Parse(Console.ReadLine());
+            AsalKontrol(kullaniciSayi);
+
+        }
+        public static void AsalKontrol(int kullaniciSayi)
+        {
+            if (kullaniciSayi<=1)
+            {
+                Console.WriteLine($"Girdiğiniz sayı {kullaniciSayi} asal değildir.");
+            }
+            bool asalMi=true;
+            for (int i = 2; i < kullaniciSayi/2; i++)
+            {
+                if (kullaniciSayi%i==0)
+                {
+                        asalMi = false;
+                    break;
+                }
+            }
+            if (asalMi)
+            {
+                Console.WriteLine($"Girdiğiniz sayi {kullaniciSayi} asaldır.");
+            }
+            else
+            {
+                Console.WriteLine($"Girdiğiniz sayi {kullaniciSayi} asal değildir.");
+            }
+        }
+    }
+}
